022dd6554403ab75190146b4c6a5a5bab3931431
DevCHW <ggoma003@naver.com>
2024-04-09 18:46:44 +0900

    html css 추가 및 핸들러

---
+++ app/app-web/src/main/java/com/coinbot/app/web/view/ExceptionAdvice.java
@@ -0,0 +1,25 @@
+package com.coinbot.app.web.view;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handle404(NoHandlerFoundException ex) {
+        return "error/404";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String except(Exception e, Model model) {
+        model.addAttribute("exception", e);
+        return "error/500";
+    }
+
+}
